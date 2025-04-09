c7a7917e5e9ac0edad46dcce42f0408073b2fa9f
Vsevolod-pop <vopopenko@gmail.com>
2025-04-10 00:33:21 +0300

    Добавьте файлы проекта.

---
+++ LW5_T3/LW5_T3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Palindrom(string K, string a = "", int i = 1)
+{
+    if (K.length() == a.length())
+    {
+        if (K == a)
+            return "True";
+        else
+            return "False";
+    }
+    else
+    {
+        a = a + K.substr(K.length() - i, 1);
+        return Palindrom(K,a,i+1);
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string a[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Введите строку - ";
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Строка - " << a[i] << " - " << Palindrom(a[i]) << endl;
+    }
+}
